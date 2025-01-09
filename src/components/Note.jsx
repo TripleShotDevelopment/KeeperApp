1f9aa0909f4c9a5564e0890f7b8c04115d9a7adc
TripleShotDevelopment <swguy270@gmail.com>
2025-01-09 12:43:53 -0600

    Stage one build

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>React JS</h1>
+        <p>React JS is a JavaScript library for building user interfaces.</p>
+    </div>
+}
+
+export default Note;
